68ae205f35cefdb56af00fc78cd981a82f8a5bbd
rdiehlmartinez <rdm@stanford.edu>
2018-07-03 07:13:08 +0000

    added model class

---
+++ model.py
@@ -0,0 +1,19 @@
+__author__ = 'Richard Diehl Martinez' 
+
+'''
+Establishes the basic CNN model for binary classification of 
+images into benign/malignant. 
+'''
+
+
+class Model(): 
+    def __init__(self): 
+        
+    
+
+
+def main():
+    pass 
+
+if __name__ == '__main__':
+    main()
