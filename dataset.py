0f24e7e95c49d5be9f73e5b88a856bf970a0ce62
rdiehlmartinez <rdm@stanford.edu>
2018-07-03 07:03:18 +0000

    updated dataset class

--- dataset.py
+++ dataset.py
@@ -6,12 +6,19 @@ Model.
 '''
 
 import numpy as np
-import pickle 
+import pickle
 from torch.utils.data import Dataset
+from PIL import Image
+import torch
+from enum import Enum 
+
+class MoleType(Enum): 
+    benign = 0 
+    malignant = 1 
  
  
- class ImageDataset(): 
-     def __init__(self, path = 'cleaned_data.pkl'):
+class ImageDataset(): 
+    def __init__(self, path = 'cleaned_data.pkl'):
         """
         Args:
             path (string): Path to the .pkl file that contains all the 
@@ -20,6 +27,7 @@ from torch.utils.data import Dataset
         self.path = path
         self.dataset = self.extract_info() # list of matrices
         self.validation = False
+        self.lookup = MoleType
 
     def __len__(self):
         '''
@@ -29,19 +37,22 @@ from torch.utils.data import Dataset
         return len(self.dataset)
         
     def __getitem__(self,idx):
-        return self.dataset[idx]
+        img_path, label = self.dataset[idx] 
+        img = Image.open(img_path)
+        label = self.lookup[label]
+        return img, label 
         
-     def extract_info(self): 
-         return pickle.load(open(self.path,'rb'))
+    def extract_info(self): 
+        return pickle.load(open(self.path,'rb'))
          
  
- def main(): 
-     print('--- Running Test for ImageDataset class ---')
-     dataset = ImageDataset()
-     print('Length of Dataset', len(dataset))  
-     index = 100 
-     print('Returning Item at index: ', index)
-     print(dataset[index])   
+def main(): 
+    print('--- Running Test for ImageDataset class ---')
+    dataset = ImageDataset()
+    print('Length of Dataset', len(dataset))  
+    index = 100 
+    print('Returning Item at index: ', index)
+    print(dataset[index])   
  
- if __name__ == '__main__': 
-    main()
+if __name__ == '__main__': 
+   main()
